1ddb06da134ea85e515746250892a53e4649c488
kec (Ke Chun-Chao) <kec@ematters.com.tw>
2015-09-09 10:42:47 +0800

    Added mail service setting

--- functions.sh
+++ functions.sh
@@ -37,3 +37,32 @@ echo "'$1' is not a valid file"
 fi
 }
 
+function() set_mail{
+#Check mail service exist or not~
+if [ -f /etc/postfix/main.cf ] ; then
+	sudo sed -i "s/myhostname =.*/myhostname = ${HOSTNAME}/;s/mydestination =.*/mydestination = ${HOSTNAME},l
+ocalhost/;s/relayhost =.*/relayhost = mail.$(hostname -d)/" /etc/postfix/main.cf
+	sudo service postfix reload
+	echo -e "Postfix reconfig done!"
+else
+	echo -e "Install Postfix to ${HOSTNAME}"
+	MAIL_SERVER=mail.$(hostname -d)
+
+cat> /tmp/debconf-mailutils.txt <<EOF
+postfix	postfix/relayhost	string	${MAIL_SERVER}
+postfix	postfix/mailname	string	$(hostname -f)
+postfix	postfix/main_mailer_type	select	Satellite system
+postfix	postfix/recipient_delim	string	+
+postfix	postfix/destinations	string	${HOSTNAME},$(hostname -f) , localhost.localdomain, localhost
+postfix	postfix/rfc1035_violation	boolean	false
+postfix	postfix/procmail	boolean	false
+postfix	postfix/mynetworks	string	127.0.0.0/8
+postfix	postfix/mailbox_limit	string	0
+postfix	postfix/protocols	select	ipv4
+EOF
+	cat /tmp/debconf-mailutils.txt |debconf-set-selections
+	sudo apt-get -y install mailutils
+	rm /tmp/debconf-mailutils.txt
+	echo -e "\n${HOSTNAME} Mail service installation done!!!\n"
+fi
+}
